aed51fc33c0a05a895b422edeb1c4e77688e8aef
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-05-05 23:32:18 +0200

    Initial commit

---
+++ Assets/Sample/SampleServer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mqtt
+{
+    
+
+    public class SampleServer : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+    }
+
+}
