c7e5a85b4437a7bc265352e082ec8dca565e9f65
Koki Ibukuro <koki.ibukuro@gmail.com>
2019-05-06 12:25:29 +0200

    Add subscribe and publish sample

--- Assets/Sample/SampleClient.cs
+++ Assets/Sample/SampleClient.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 using MQTTnet;
@@ -13,6 +15,7 @@ public class SampleClient : MonoBehaviour
     int port = 1883;
 
     IMqttClient client;
+    StringBuilder sb = new StringBuilder();
 
     async void Start()
     {
@@ -27,6 +30,13 @@ public class SampleClient : MonoBehaviour
 
         await client.ConnectAsync(options);
         Debug.Log("connected to the server");
+
+        var topic = new TopicFilterBuilder()
+            .WithTopic("my/test")
+            .Build();
+        await client.SubscribeAsync("/my/test");
+
+        Debug.Log("Subscribed");
     }
 
     async void OnDestroy()
@@ -40,7 +50,18 @@ public class SampleClient : MonoBehaviour
         Debug.Log("disconnected");
     }
 
-
+    async void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            var msg = new MqttApplicationMessageBuilder()
+                .WithTopic("/my/test")
+                .WithPayload("hgoehoge")
+                .WithExactlyOnceQoS()
+                .Build();
+            await client.PublishAsync(msg);
+        }
+    }
 
     private void OnConnected(object sender, MqttClientConnectedEventArgs e)
     {
@@ -54,7 +75,15 @@ public class SampleClient : MonoBehaviour
 
     private void OnApplicationMessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
     {
-        Debug.Log($"OnApplicationMessageReceived: {e}");
+        sb.Clear();
+        sb.AppendLine("Message:");
+        sb.AppendFormat("ClientID: {0}\n", e.ClientId);
+        sb.AppendFormat("Topic: {0}\n", e.ApplicationMessage.Topic);
+        sb.AppendFormat("Payload: {0}\n", Encoding.UTF8.GetString(e.ApplicationMessage.Payload));
+        sb.AppendFormat("QoS: {0}\n", e.ApplicationMessage.QualityOfServiceLevel);
+        sb.AppendFormat("Retain: {0}\n", e.ApplicationMessage.Retain);
+        
+        Debug.Log(sb);
     }
 }
 
